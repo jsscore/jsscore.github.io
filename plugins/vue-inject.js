09da3c3d58452ab706355c799dd915cadf36cbea
sunshine-SJ <17600883526@163.com>
2021-08-23 17:29:18 +0800

    update

--- plugins/vue-inject.js
+++
@@ -1,8 +0,0 @@
-import Vue from 'vue'
-import dayjs from 'dayjs'
-import api from '../api'
-import storage from './localStorage.js'
-
-Vue.prototype.$api = api
-Vue.prototype.$storage = new storage()
-Vue.prototype.$dayjs = dayjs
